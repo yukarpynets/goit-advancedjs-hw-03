390b1ded0f633a6a6bcf44e942e6e5def960dcd3
Yulia Karpynets <yukarpynets90@gmail.com>
2026-04-15 10:59:33 +0200

    add axios library

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,15 @@
+import axios from 'axios';
+
 export const getImagesByQuery = query => {
-  const params = new URLSearchParams({
+  const params = {
     key: '55427334-75cc24dd7c9d859437f7d1aed',
     q: query.trim(),
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
+  };
 
-  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-
-    return response.json();
-  });
+  return axios
+    .get('https://pixabay.com/api/', { params })
+    .then(response => response.data);
 };
